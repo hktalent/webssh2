0905fd6041fd5d4ce3b06b845abac0f5b4a3a77f
51pwn <18223385+hktalent@users.noreply.github.com>
2022-04-23 21:00:55 +0800

    add app2

---
+++ app2/src/App.vue
@@ -0,0 +1,193 @@
+<template>
+  <div id="app">
+
+    <el-container>
+      <el-header>
+        <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleCommand"
+          background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+          <el-menu-item index="1">处理中心</el-menu-item>
+          <el-submenu index="2">
+            <template slot="title" index="2-0">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项1</el-menu-item>
+              <el-menu-item index="2-4-2">选项2</el-menu-item>
+              <el-menu-item index="2-4-3">选项3</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+        </el-menu>
+      </el-header>
+      <el-container>
+      <el-aside width="70px">
+      <el-scrollbar  class="myscrollbar">
+        <el-menu default-active="1-4-1" collapse="true" @onmouseup="this.collapse=true" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span slot="title">导航一</span>
+            </template>
+            <el-menu-item-group>
+              <span slot="title">分组一</span>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <span slot="title">选项4</span>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+        </el-scrollbar>
+      </el-aside>
+      <el-main style="height:calc(-100px + 100vh)">
+
+
+
+        <el-tabs type="border-card"  style="height:100%;flex-grow:1;">
+          <el-tab-pane label="用户管理">
+            <el-button @click="drawer = true" type="primary" style="margin-left: 16px;">
+              点我打开
+            </el-button>
+            <el-drawer :visible.sync="drawer" :direction="direction">
+              <span>我来啦!</span>
+            </el-drawer>
+          </el-tab-pane>
+          <el-tab-pane label="配置管理">
+
+            <template>
+              <el-select v-model="value" filterable multiple collapse-tags placeholder="请选择" :filter-method="fltFunc">
+                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value">
+                </el-option>
+              </el-select>
+            </template>
+
+
+          </el-tab-pane>
+          <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+          <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+        </el-tabs>
+
+      </el-main></el-container>
+      <el-footer>Footer</el-footer>
+    </el-container>
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  runtimeCompiler: true,
+  data() {
+    return {
+      isCollapse: true,
+      activeIndex2: '2-0',
+      options: [{
+        value: '选项1',
+        label: '黄金糕'
+      }, {
+        value: '选项2',
+        label: '双皮奶'
+      }, {
+        value: '选项3',
+        label: '蚵仔煎'
+      }, {
+        value: '选项4',
+        label: '龙须面'
+      }, {
+        value: '选项5',
+        label: '北京烤鸭'
+      }],
+      value: '',
+      drawer: false,
+      direction: 'rtl',
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    handleCommand(command) {
+    },
+    handleClose1(done) {
+      done()
+    },
+    // 下拉列表过滤
+    fltFunc(s) {
+      if ("" != s) {
+        s = String(s).toLowerCase()
+        if (!this.oldOtp) this.oldOtp = this.options
+        this.options = this.oldOtp.filter(item => {
+          return item.label.toLowerCase().indexOf(s) > -1 || item.value.toLowerCase().indexOf(s) > -1
+        });
+      } else this.options = this.oldOtp;
+    },
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style>
+@import url("//unpkg.com/element-ui@2.15.7/lib/theme-chalk/index.css");
+
+body{margin:0}
+.el-header{width:100vw;padding:0;}
+
+.el-drawer{z-index:99999;height:calc(-100px + 100vh);margin-top:60px}
+.el-aside{float:left;position:absolute;z-index:999}
+.el-main{margin-left:50px;}
+
+.myscrollbar{
+	 height: 100%;
+	}
+.myscrollbar > .el-scrollbar__wrap{
+  overflow-x: hidden;
+}
+
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409EFF;
+}
+
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both
+}
+</style>
