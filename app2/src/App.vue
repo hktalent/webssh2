6785471fb3f157ac99a6b45fdc578812f40c5c4f
51pwn <18223385+hktalent@users.noreply.github.com>
2022-04-23 21:59:54 +0800

    u

--- app2/src/App.vue
+++ app2/src/App.vue
@@ -23,34 +23,38 @@
       <el-container>
       <el-aside width="70px">
       <el-scrollbar  class="myscrollbar">
-        <el-menu default-active="1-4-1" collapse="true" @onmouseup="this.collapse=true" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
-          <el-submenu index="1">
+        <el-menu default-active="2-4-1" collapse="true" class="el-menu-vertical-demo" @click="handClickMm"  @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+          <el-menu-item index="1">
+           <i class="el-icon-right-open"></i><span slot="title">打开菜单导航</span>
+          </el-menu-item>
+
+          <el-submenu index="2">
             <template slot="title">
               <i class="el-icon-location"></i>
               <span slot="title">导航一</span>
             </template>
             <el-menu-item-group>
               <span slot="title">分组一</span>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
+              <el-menu-item index="2-1">选项1</el-menu-item>
+              <el-menu-item index="2-2">选项2</el-menu-item>
             </el-menu-item-group>
             <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
+              <el-menu-item index="2-3">选项3</el-menu-item>
             </el-menu-item-group>
-            <el-submenu index="1-4">
+            <el-submenu index="2-4">
               <span slot="title">选项4</span>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
+              <el-menu-item index="2-4-1">选项1</el-menu-item>
             </el-submenu>
           </el-submenu>
-          <el-menu-item index="2">
+          <el-menu-item index="3">
             <i class="el-icon-menu"></i>
             <span slot="title">导航二</span>
           </el-menu-item>
-          <el-menu-item index="3" disabled>
+          <el-menu-item index="4" disabled>
             <i class="el-icon-document"></i>
             <span slot="title">导航三</span>
           </el-menu-item>
-          <el-menu-item index="4">
+          <el-menu-item index="5">
             <i class="el-icon-setting"></i>
             <span slot="title">导航四</span>
           </el-menu-item>
@@ -145,6 +149,9 @@ export default {
     },
     handleClose(key, keyPath) {
       console.log(key, keyPath);
+    },
+    handClickMm(){
+      console.log(arguments)
     }
   }
 }
@@ -155,6 +162,10 @@ export default {
 
 body{margin:0}
 .el-header{width:100vw;padding:0;}
+
+.el-menu-demo{
+padding-left:65px
+}
 
 .el-drawer{z-index:99999;height:calc(-100px + 100vh);margin-top:60px}
 .el-aside{float:left;position:absolute;z-index:999}
--- app2/src/main.js
+++ app2/src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
 
 import App from './App.vue'
 // import VueRouter from 'vue-router'
